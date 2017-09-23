877fe2848c812fca35003eba07adb8b82551d454
sbensiali <salim.bensiali@nicta.com.au>
2017-09-23 20:01:08 +1000

    implement auto update of the preview

--- src/App.js
+++ src/App.js
@@ -1,45 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const InputField = ({ name, type="text" }) => (
+const InputField = ({ title, type="text", onChange }) => (
   <div className={`wrapper-input-$name.replace(" ", "-")`}>
-    <label>{name}</label>
-    <input type={type} />
+    <label>{title}</label>
+    <input type={type} onChange={onChange} />
   </div>
 );
 
-const FieldGroup = ({ title, fields }) => (
+const FieldGroup = ({ title, fields, changeHandler }) => (
   <fieldset className={title.replace(" ", "-")}>
       <h3>{title}</h3>
-      {fields.map(({ name, type="text" }, index) => (
-        <InputField key={index} name={name} type={type} />
+      {fields.map(({ name, title, type="text" }) => (
+        <InputField key={name} title={title || name} type={type} onChange={changeHandler.bind(this, name)} />
       ))}
     </fieldset>
 );
 
+const HcardPreview = ({ title, givenName="", surname="", email, phone, houseNameOrNumber, street, suburb, state, postCode, country }) => (
+  <div className="hcard-preview">
+    <h2>{title}</h2>
+    <img className="hcard-preview-avatar" alt="Avatar" />
+    <p className="hcard-preview-name">
+      {givenName} {surname}
+    </p>
+    <div className="hcard-preview-email">
+      <span>email</span>{email}
+    </div>
+    <div className="hcard-preview-phone">
+      <span>phone</span>{phone}
+    </div>
+    <div className="hcard-preview-address">
+      <span>address</span>
+      {houseNameOrNumber} {street}
+      <br /> {suburb}, {state}
+    </div>
+    <div className="hcard-preview-postcode">
+      <span>postcode</span>{postCode}
+    </div>
+    <div className="hcard-preview-country">
+      <span>country</span>{country}
+    </div>
+  </div>
+);
+
 class App extends Component {
+  state = {};
+
   fieldGroups = [{
     title: "Personal details",
     fields: [
-      { name: "Given name" },
-      { name: "Surname" },
-      { name: "Email", type: "email" },
+      { name: "givenName", title: "Given name" },
+      { name: "surname" },
+      { name: "email" , type: "email" },
       { name: "phone" },
-      { name: "Given name", type: "text" },
     ]
   }, {
     title: "Address",
     fields: [
-      { name: "House number or #" },
-      { name: "Stree" },
-      { name: "Suburb" },
-      { name: "State" },
-      { name: "Post code" },
+      { name: "houseNameOrNumber", title: "House name or #" },
+      { name: "street" },
+      { name: "suburb" },
+      { name: "state" },
+      { name: "postcode", type: "number" },
       { name: "Country" },
     ]
+  }];
+
+  handleChange = (fieldName, e) => {
+    this.setState({[fieldName]: e.target.value});
+    console.log(fieldName, e.target.value);
   }
-  ];
 
   render() {
     return (
@@ -47,18 +78,16 @@ class App extends Component {
         <div className="hcard-builder">
           <h1 className="App-title">hCard builder</h1>
           {this.fieldGroups.map((props, index) => (
-            <FieldGroup key={index} {...props} />
+            <FieldGroup key={index} {...props} changeHandler={this.handleChange} />
             ))}
           <div className="App-controls">
             <button>Upload Avatar</button>
             <button>Create hCard</button>
           </div>
         </div>
-        <div className="hcard-preview">
-          <h2>Hcard preview</h2>
-          <img className="avatar" />
-          <p className="name"></p>
-        </div>
+        <HcardPreview
+          title="Hcard preview"
+          {...this.state} />
       </div>
     );
   }
